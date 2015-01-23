1205bcfba8ee2252b978d71f68067cce0a48cc5c
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-01-23 21:09:04 +0000

    Add: grunt file and package

---
+++ GruntFile.js
@@ -0,0 +1,40 @@
+module.exports = function( grunt ) {
+
+	grunt.initConfig({
+		"6to5": {
+			"dist": {
+				"files": {
+					"dist/NavigationMesh.js": "dist/NavigationMesh-es6.js"
+				}
+			},
+			"options": {
+				"sourceMap": true
+			}
+		},
+		"concat": {
+			"dist": {
+				"dest": "dist/NavigationMesh-es6.js",
+				"src": [ "src/**/*.js" ]
+			}
+		},
+		"pkg": grunt.file.readJSON("package.json"),
+		"watch": {
+			"dist": {
+				"files": [ "src/**/*.js" ],
+				"options": {
+					"interrupt": true,
+					"livereload": true
+				},
+				"tasks": [ "default" ]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-6to5");
+
+	// Default task(s).
+	grunt.registerTask("default", [ "concat:dist", "6to5:dist" ]);
+
+};
