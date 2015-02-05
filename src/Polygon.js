c06aece792401d74092381fdf724108d3e350829
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-02-05 19:30:42 +0000

    Fix: polygon has no points when started wont be rendered

--- src/Polygon.js
+++ src/Polygon.js
@@ -42,6 +42,7 @@ class Polygon extends DisplayObject {
 	}
 
 	render( ctx ){
+		if( this.points.length === 0 ) return;
 		ctx.fillStyle = this.fill;
 		ctx.beginPath();
 		ctx.moveTo( this.points[0].x, this.points[0].y );
