26b21a4d834f77f972199871db5dcdd3d3600dee
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-02-05 07:59:14 +0000

    Add: centroid to polygon

--- src/Polygon.js
+++ src/Polygon.js
@@ -1,7 +1,39 @@
 class Polygon extends DisplayObject {
 
 	centroid(){
+		var centroid = new Point();
+		var pts = this.points;
+		var signedArea = 0;
+		var area = 0;
+		var x0 = 0;
+		var y0 = 0;
+		var x1 = 0;
+		var y1 = 0;
 
+		var calculate = function( pt1, pt2 ){
+			x0 = pt1.x;
+			y0 = pt1.y;
+			x1 = pt2.x;
+			y1 = pt2.y;
+
+			area = x0*y1 - x1*y0;
+			signedArea += area;
+			centroid.x += ( x0 + x1 ) * area;
+			centroid.y += ( y0 + y1 ) * area;
+		};
+
+		var i = -1, l = pts.length-1;
+		while( ++i < l ){
+			calculate( pts[i], pts[i+1] );
+		}
+
+		calculate( pts[i], pts[0] );
+
+		signedArea = ( signedArea * 0.5 ) * 6;
+		centroid.x /= signedArea;
+		centroid.y /= signedArea;
+
+		return centroid;
 	}
 
 	constructor( points ){
