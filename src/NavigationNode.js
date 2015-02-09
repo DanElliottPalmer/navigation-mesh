a45a958b9bd8a546b273f0149baf23ac2455c746
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-02-09 13:41:18 +0000

    Add: id to the nodes

--- src/NavigationNode.js
+++ src/NavigationNode.js
@@ -1,7 +1,9 @@
+let NavigationNodeID = 0;
 class NavigationNode {
 
 	constructor( triangle ){
 		let { x, y } = triangle.centroid;
+		this._id = NavigationNodeID++;
 		this.neighbours = [];
 		this.triangle = triangle;
 		this.x = x >>> 0;
