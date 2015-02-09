028f7a7fe499dfcd98f7ffebb570632583c2fa67
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-02-09 17:25:47 +0000

    Add: PriorityQueue

---
+++ src/PriorityQueue.js
@@ -0,0 +1,68 @@
+class PriorityQueueItem {
+
+	constructor( value, priority ){
+		this.priority = priority;
+		this.value = value;
+	}
+
+}
+
+/**
+ * http://algs4.cs.princeton.edu/24pq/
+ */
+class PriorityQueue {
+
+	constructor(){
+		this.heap = [ null ];
+	}
+
+	get length(){
+		return this.heap.length - 1;
+	}
+
+	less( a, b ){
+		return this.heap[ a ].priority < this.heap[ b ].priority;
+	}
+
+	pop(){
+		let value = this.heap[ 1 ].value;
+		this.heap[ 1 ] = this.heap.pop();
+		this.sink( 1 );
+		return value;
+	}
+
+	push( value, priority ){
+		let item = new PriorityQueueItem( value, priority );
+		this.heap.push( item );
+		this.swim( this.length );
+	}
+
+	sink( k ){
+		let j = k << 1;
+		while( j <= this.length ){
+			if( j < this.length && this.less( j, j + 1 ) ){
+				j++;
+			}
+			if( !this.less( k, j ) ) break;
+			this.swap( k, j );
+			k = j;
+		}
+	}
+
+	swap( a, b ){
+		let tmp = this.heap[ a ];
+		this.heap[ a ] = this.heap[ b ];
+		this.heap[ b ] = tmp;
+	}
+
+	swim( k ){
+		let j = 0;
+		while( k > 1 ){
+			j = k >> 1;
+			if( !this.less( j, k ) ) break;
+			this.swap( k, j );
+			k = j;
+		}
+	}
+
+}
