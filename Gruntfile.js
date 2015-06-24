3c4d485d4ae2cbb5538e4af7b9dd01fe2fb35a89
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-06-24 23:04:47 +0100

    ADD: grunt and package

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		"babel": {
+			"dev": {
+				"files": [{
+					"cwd": "./src/",
+					"expand": true,
+					"dest": "./tmp/",
+					"src": [ "**/*.js" ]
+				}]
+			}
+		},
+		"browserify": {
+			"dev": {
+				"files": {
+					"dist/navigation-mesh.js": [ "tmp/navigation-mesh.js" ]
+				},
+				"options": {
+					"browserifyOptions": {
+						"standalone": "NavigationMesh"
+					}
+				}
+			}
+		},
+		"pkg": grunt.file.readJSON("package.json"),
+		"watch": {
+			"dev-js": {
+				"files": [ "src/**/*" ],
+				"tasks": [ "babel:dev", "browserify:dev" ]
+			},
+			"options": {
+				"livereload": true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-browserify");
+	grunt.loadNpmTasks("grunt-babel");
+
+};
