35cb3091ff112dc390fc902552c7880d469139c6
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-06-24 23:42:24 +0100

    ADD: some meat to NavigationMesh. constructor and Polygon class

--- src/NavigationMesh.js
+++ src/NavigationMesh.js
@@ -1,7 +1,18 @@
 import Point from "./Point";
+import Polygon from "./Polygon";
 
-class NavigationMesh {}
+class NavigationMesh {
+
+	constructor( points, holes = null ){
+		this.graph = null;
+		this.shape = new Polygon( points );
+	}
+
+	route( start, end ){}
+
+}
 
 NavigationMesh.Point = Point;
+NavigationMesh.Polygon = Polygon;
 
 export default NavigationMesh;
