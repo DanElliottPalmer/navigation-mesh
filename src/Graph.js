519dc6b77c0b11a1dd8784c86da8dc834dfd8dfd
DanElliottPalmer <danelliottpalmer@gmail.com>
2015-07-05 17:22:16 +0100

    ADD: pathfinding

--- src/Graph.js
+++ src/Graph.js
@@ -1,5 +1,6 @@
 import Link from "./Link";
 import Node from "./Node";
+import PriorityQueue from "./PriorityQueue";
 
 class Graph {
 
@@ -51,7 +52,80 @@ class Graph {
 		this.nodes.delete( node.toString() );
 	}
 
-	route( start, end ){}
+	route( startNode, endNode ){
+
+		if( !startNode ) return false;
+		if( !endNode ) return false;
+
+		// Pathfinding
+		// Priority queue to determine which nodes to check
+		let openQueue = new PriorityQueue( function( a, b ){
+			return this.heap[ a ].priority > this.heap[ b ].priority;
+		} );
+		// Priority counter
+		let priority = 0;
+		// Keeps track of points we've visited
+		let closedList = new Map();
+		// Keeps track of the cost of the points we've visited
+		let costHistory = new Map();
+		// The current node
+		let currentNode = null;
+		// The next node
+		let nextNode = null;
+		// The totaled new cost
+		let newCost = 0;
+		// Heuristic function
+		let heuristic = ( a, b ) => Math.abs( a.x - b.x ) + Math.abs( a.y - b.y );
+
+
+		// Add the start point to the queue
+		openQueue.push( startNode, 0 );
+
+		// Reset the closedList and costHistory
+		closedList.set( startNode.toString(), null );
+		costHistory.set( startNode.toString(), 0 );
+
+		// Keep looping till we have emptied our queue
+		while( openQueue.length > 0 ){
+			currentNode = openQueue.pop();
+
+			// If the currentNode is the endNode, stahp
+			if( currentNode === endNode ) break;
+
+			let currentLinks = this.getLinksContainingNode( currentNode );
+			currentLinks.forEach( neighbour => {
+
+				newCost = costHistory.get( currentNode.toString() ) + neighbour.length;
+				nextNode = neighbour.getOtherNode( currentNode );
+
+				/**
+				 * Check if we haven't already visited the point and add it. If we
+				 * have visited and the new cost is less, update the darn thing
+				 */
+				if( !costHistory.has( nextNode.toString() ) || newCost < costHistory.get( nextNode.toString() ) ){
+					costHistory.set( nextNode.toString(), newCost );
+					priority = newCost + heuristic( endNode.position, nextNode.position );
+					openQueue.push( nextNode, priority );
+					closedList.set( nextNode.toString(), currentNode );
+				}
+
+			});
+
+		}
+
+		return walkClosedList( closedList, startNode, endNode );
+
+		function walkClosedList( closedList, startNode, endNode ){
+			let currentNode = endNode;
+			let path = [ currentNode.position.clone() ];
+			while( currentNode !== startNode ){
+				currentNode = closedList.get( currentNode.toString() );
+				path.unshift( currentNode.position.clone() );
+			}
+			return path;
+		}
+
+	}
 
 	static createLink( node1, node2 ){
 		return new Link( node1, node2 );
--- src/NavigationMesh.js
+++ src/NavigationMesh.js
@@ -69,7 +69,100 @@ class NavigationMesh {
 
 	}
 
-	route( start, end ){}
+	route( start, end ){
+
+		if( !start ) return false;
+		if( !end ) return false;
+
+		let startPoint = start.clone();
+		let endPoint = end.clone();
+
+		// Check points are in the shape
+		if( !this.shape.contains( startPoint ) ) return false;
+		if( !this.shape.contains( endPoint ) ) return false;
+
+		// Check if the two points can see each other. If so, we can skip
+		// adding them to the graph network
+		let los = [ startPoint, endPoint ];
+		let intersection = false;
+		this.shape.vertices.forEach( vertex => {
+			if( intersection ) return;
+			intersection = lineIntersection( los, vertex, false );
+		});
+		if( !intersection ){
+
+			// Check the half point is inside the shape
+			let halfX = ( endPoint.x - startPoint.x ) / 2;
+			let halfY = ( endPoint.y - startPoint.y ) / 2;
+			let halfPoint = new Point( startPoint.x + halfX, startPoint.y + halfY );
+
+			if( this.shape.contains( halfPoint ) ){
+				return [ startPoint, endPoint ];
+			}	
+			
+		}
+
+		// Add start and end points to the graph
+		let startNode = Graph.createNode( startPoint );
+		let endNode = Graph.createNode( endPoint );
+		this.graph.addNode( startNode );
+		this.graph.addNode( endNode );
+
+		// Add links to nodes
+		this.graph.nodes.forEach( node => {
+
+			// Dont need to check the node if it is our startPoint or endPoint
+			if( node === startNode || node === endNode ) return;
+
+			// We're going to check both nodes in this one loop to save an extra loop
+			let losStart = [ startNode.position, node.position ];
+			let losEnd = [ endNode.position, node.position ];
+			let intersectionStart = false;
+			let intersectionEnd = false;
+			this.shape.vertices.forEach( vertex => {
+				// Start node check
+				if( !intersectionStart ){
+					intersectionStart = lineIntersection( losStart, vertex, false );
+				}
+				// End node check
+				if( !intersectionEnd ){
+					intersectionEnd = lineIntersection( losEnd, vertex, false );
+				}
+			});
+			// If both nodes have intersected a line, move onto the next node
+			if( intersectionStart && intersectionEnd ) return;
+
+			// Check that the los is not outside the shape
+			if( !intersectionStart ){
+				let halfX = ( node.position.x - startNode.position.x ) / 2;
+				let halfY = ( node.position.y - startNode.position.y ) / 2;
+				let halfPoint = new Point( startNode.position.x + halfX, startNode.position.y + halfY );
+
+				if( this.shape.contains( halfPoint ) ){
+					this.graph.addLink( Graph.createLink( startNode, node ) );
+				}				
+			}
+			if( !intersectionEnd ){
+				let halfX = ( node.position.x - endNode.position.x ) / 2;
+				let halfY = ( node.position.y - endNode.position.y ) / 2;
+				let halfPoint = new Point( endNode.position.x + halfX, endNode.position.y + halfY );
+
+				if( this.shape.contains( halfPoint ) ){
+					this.graph.addLink( Graph.createLink( endNode, node ) );
+				}				
+			}
+
+		});
+
+		let path = this.graph.route( startNode, endNode );
+
+		// Clean up - remove start and end nodes from graph
+		this.graph.removeNode( startNode );
+		this.graph.removeNode( endNode );
+
+		return path;
+
+	}
 
 }
 
